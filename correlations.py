72f6462e1e79adf3f0063ce54d75051fd3d1bb14
Adrian Sibiga <adriansibiga@gmail.com>
2023-04-17 21:27:34 -0400

    Started working on correlations.py, using datetime to make it easy

---
+++ correlations.py
@@ -0,0 +1,37 @@
+import csv
+import string
+import time
+import datetime
+
+purchase_file = 'test_samples/test_sample_5.csv'
+stock_data_file = 'stock_data/AFBI___2023-02-16_12-27-07.csv'
+
+
+# finds filing dates of each purchase in the purchase file and turns them into strings
+with open(purchase_file, 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        filing_date = row[0]
+        
+        # initiate lists
+        date = []
+        clock = []
+        
+        # gathers the characters of the filing date
+        for i in range(0,10):
+            date.append(filing_date[i])
+        for i in range(11,19):
+            clock.append(filing_date[i])
+        
+        # turns the lists of characters into strings
+        date = "".join(date)
+        clock = "".join(clock)
+        
+        # puts the filing date into the datetime system so that we can do easy date manipulations by days, months, hours, etc.
+        filing_date = datetime.datetime.fromisoformat(date+"T"+clock)
+
+
+# with open(stock_data_file, 'r') as csvfile:
+#     datareader = csv.reader(csvfile)
+#     for row in datareader:
+        
--- debugging.py
+++ debugging.py
@@ -1,19 +1,9 @@
-import requests
-import csv
-import string
+from datetime import datetime
+from datetime import timedelta
 
-ticker_symbol = "LGTO"
-filing_date = "###"
+a = datetime.fromisoformat('2011-11-04T00:05')
+a = datetime(2011, 11, 4, 0, 5)
+b = timedelta(days=10)
 
-csv_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=' + ticker_symbol + '&interval=15min&slice=year1month1&apikey=OHLMGAI6A5CXX9Q3'
-csv_filename = ticker_symbol + "___" + filing_date + ".csv"
-directory = "C:/Stock-Trading-Algorithm/" + csv_filename
 
-with requests.Session() as s:
-    download = s.get(csv_url)
-    decoded_content = download.content.decode('utf-8')
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    with open(directory, "w", newline = "") as f:
-        writer = csv.writer(f)
-        writer.writerows(my_list)
+print(a + b)
